b15f7fe25688b7d5d55c7e55bdb013cc781326f2
Sreeram <hotshotsreeram@gmail.com>
2023-01-11 13:36:52 -0500

    adding remote backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "my-tf-sr-first-bucket"
+    key = "practice/jenkins-job/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
