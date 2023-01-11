24a0ac01a7976558fe63ac6204aee22735cdae4e
Sreeram <hotshotsreeram@gmail.com>
2023-01-11 15:47:12 -0500

    updating user data to clone github repo

--- userdata.sh
+++ userdata.sh
@@ -5,3 +5,5 @@ sudo apt install nginx -y
 echo "<h1>$(curl https://api.prod.rest/?format=text)</h1>" >  /usr/share/nginx/html/index.html 
 systemctl enable nginx
 systemctl start nginx
+cd /
+git clone https://github.com/hotshotsreeram/jenkins-job.git
