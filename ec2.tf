86ee73b771a164063019fa87070c33a52f144f16
Sreeram <hotshotsreeram@gmail.com>
2023-01-19 15:34:51 -0500

    updating launch template

--- ec2.tf
+++ ec2.tf
@@ -30,7 +30,6 @@ resource "aws_launch_template" "hello-world" {
     name = var.profile
   }
   image_id = aws_ami_from_instance.practice.id
-  instance_initiated_shutdown_behavior = "terminate"
   instance_type = var.instance_type
   key_name = var.key
   vpc_security_group_ids = var.security_group
