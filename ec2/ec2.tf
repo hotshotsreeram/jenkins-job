310096279eae2eacfd36c836129e080784a9319d
Sreeram <hotshotsreeram@gmail.com>
2023-01-24 17:15:10 -0500

    updating data field in ami.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -15,7 +15,6 @@ resource "aws_instance" "practice_instance" {
     # starting with the distinct index number 0 and corresponding to this instance.
     Name = "my-machine"
   }
-  
   lifecycle {
     create_before_destroy = true
   }
