61992da5579666fc95af51fc36b7c579661642f9
Sreeram <hotshotsreeram@gmail.com>
2023-01-24 16:55:32 -0500

    updating data field in ami.tf

--- ami/ami.tf
+++ ami/ami.tf
@@ -1,9 +1,10 @@
 data "terraform_remote_state" "instance"{
-    backend = "s3" {
+    backend = "s3" 
+    config = {
         bucket = "my-tf-sr-first-bucket"
         key = "practice/jenkins-job/ec2/terraform.tfstate"
         region = "us-east-1"
-  }
+    }
 }
 
 resource "aws_ami_from_instance" "practice" {
